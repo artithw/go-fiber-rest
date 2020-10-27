dc24d570fb4a3d15c2ad56d6559fb778766a20cb
artithw <artith.w@gmail.com>
2020-10-27 17:45:25 +0700

    add model fixlogs

--- handler/fixlog.go
+++ handler/fixlog.go
@@ -7,49 +7,49 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-// GetAllProducts query all products
+// GetAllProducts query all fixlogs
 func GetAllFixlogs(c *fiber.Ctx) error {
 	db := database.DB
-	var products []model.Product
-	db.Find(&products)
-	return c.JSON(fiber.Map{"status": "success", "message": "All products", "data": products})
+	var fixlogs []model.Fixlogs
+	db.Find(&fixlogs)
+	return c.JSON(fiber.Map{"status": "success", "message": "All products", "data": fixlogs})
 }
 
-// GetProduct query product
+// GetProduct query fixlog
 func GetFixlog(c *fiber.Ctx) error {
 	id := c.Params("id")
 	db := database.DB
-	var product model.Product
-	db.Find(&product, id)
-	if product.Title == "" {
+	var fixlog model.Fixlogs
+	db.Find(&fixlog, id)
+	if fixlog.Title == "" {
 		return c.Status(404).JSON(fiber.Map{"status": "error", "message": "No product found with ID", "data": nil})
 
 	}
-	return c.JSON(fiber.Map{"status": "success", "message": "Product found", "data": product})
+	return c.JSON(fiber.Map{"status": "success", "message": "Product found", "data": fixlog})
 }
 
-// CreateProduct new product
+// CreateProduct new fixlog
 func CreateFixlog(c *fiber.Ctx) error {
 	db := database.DB
-	product := new(model.Product)
-	if err := c.BodyParser(product); err != nil {
+	fixlog := new(model.Fixlogs)
+	if err := c.BodyParser(fixlog); err != nil {
 		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Couldn't create product", "data": err})
 	}
-	db.Create(&product)
-	return c.JSON(fiber.Map{"status": "success", "message": "Created product", "data": product})
+	db.Create(&fixlog)
+	return c.JSON(fiber.Map{"status": "success", "message": "Created product", "data": fixlog})
 }
 
-// DeleteProduct delete product
+// DeleteProduct delete fixlog
 func DeleteFixlog(c *fiber.Ctx) error {
 	id := c.Params("id")
 	db := database.DB
 
-	var product model.Product
-	db.First(&product, id)
-	if product.Title == "" {
+	var fixlog model.Fixlogs
+	db.First(&fixlog, id)
+	if fixlog.Title == "" {
 		return c.Status(404).JSON(fiber.Map{"status": "error", "message": "No product found with ID", "data": nil})
 
 	}
-	db.Delete(&product)
+	db.Delete(&fixlog)
 	return c.JSON(fiber.Map{"status": "success", "message": "Product successfully deleted", "data": nil})
 }
